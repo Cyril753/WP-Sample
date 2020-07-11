245e2e0783a3e064854e8871ef03f40a78f51f0c
cyril <cyrilwang012@gmail.com>
2020-07-10 23:22:04 -0700

    first commit

---
+++ header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php wp_head(); ?>
+  </head>
+  <body>
+    <header class="header">
+      <div class="header__wrap">
+        <div class="header__logo">
+          <a href="#"><strong>YUYOSOFT</strong> INNOVATIONS INC</a>
+        </div>
+        <nav class="header__nav">
+          <a href="#" class="header__nav-item header__nav-item_status_active">Home</a>
+          <a href="#" class="header__nav-item">About Us</a>
+          <a href="#" class="header__nav-item">Mobile Game</a>
+          <a href="#" class="header__nav-item">Music Labels</a>
+          <span class="header__nav-item"><i class="fa fa-search" aria-hidden="true"></i></span>
+        </nav>
+      </div>
+      
+      
+        <!-- <div class="container">
+          <div class="logo">
+            <h1 class="logo-text float-left">
+              <a href="#"><strong>YUYOSOFT</strong> INNOVATIONS INC</a>
+            </h1>
+          </div>
+          <span class="js-search-trigger site-header__search-trigger"></span>
+          <div class="site-header__menu group">
+            <nav class="main-navigation">
+              <ul>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Mobile Game</a></li>
+                <li><a href="#">Music Labels</a></li>
+              </ul>
+            </nav>
+            <div class="site-header__util">
+              <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+            </div>
+          </div>
+        </div> -->
+    </header>
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php get_header(); ?>
+
+ 
+<div class="page-banner">
+    <div class="page-banner__bg-image"></div>
+    <div class="page-banner__text">
+        <h1 class="headline">YUYOSOFT INNOVATIONS INC</h1>
+        <p class="headline">Vancouver • Tokyo</p>
+        <h2 class="headline">Designing and building mobile games</h2>
+        <h2 class="headline">Producing music and media in English and Japanese</h2>
+    </div>    
+</div>
+<div class="full-width-split">
+    <div class="full-width-split__one">
+        <div class="full-width-split__one-inner">
+            <p>Our mission is twofold, and we therefore have two groups within our organization, working on the following:</p>
+            <ul>
+                <li>Mobile games: We design, produce and sell games for mobile devices.</li>
+                <li>Music: We produce and sell electro pop music in the English and Japanese markets. This includes finding and developing young Japanese singers. We also produce interactive media and movies incorporating our music.</li>
+            </ul>
+        </div>
+    </div>
+    <div class="full-width-split__two">
+        <div class="full-width-split__two-inner">
+            <p>Yu-Yo- means “useful” in the Japanese language. Our vision is to produce mobile games, music and media that are truly useful to our customers.</p>
+            <p>For enquiries, please contact us <strong><a href="#">here</a></strong>. No sales solicitations or spam, please.</p>
+        </div>
+    </div>
+</div>
+    
+  <?php 
+
+  get_footer();
+
+?>
